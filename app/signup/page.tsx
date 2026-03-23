6e447f15e7eb1f721f1af4db8f60f29a96ec2c79
Diego Carvalho <95982064+Diegocchaves@users.noreply.github.com>
2026-03-23 11:16:57 -0600

    upgrade the UI of the sign up component.

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -38,24 +38,39 @@ export default function SignUpPage() {
   }
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-[url('/bc-logo.svg')] bg-cover bg-center bg-no-repeat h-screen w-[900] flex justify-center items-center">
-        <div className="flex flex-col items-center justify-center w-96 h-[500] gap-6 p-6 border rounded-lg shadow-2xl w-ull bg-white/30 backdrop-blur-md">
-          <h1 className="text-xl font-semibold text-gray-900">
-            Create account
-          </h1>
+      <div className="bg-[url('/bc-logo-1.svg')] bg-cover bg-center bg-no-repeat h-screen w-[900] flex justify-center items-center ">
+        <div className="flex flex-col justify-center items-center w-96 h-[500] gap-6 p-6 border rounded-lg shadow-2xl w-ull bg-white/30 backdrop-blur-xl">
+          <div className="flex flex-row items-center justify-center gap-1">
+            <img
+              src="/bc-logo-1.svg"
+              alt="Breadcrumbz logo"
+              className="w-20 h-20"
+            />{" "}
+            <h1 className="text-3xl font-bold text-gray-800 ">BreadCrumbz</h1>
+          </div>
+          <h2 className="flex items-center justify-center text-[15px] text-gray-800 font-extralight">
+            Start your jorney with BreadCrumbz
+          </h2>
           <div className="flex flex-col items-center justify-center w-full gap-4 p-4 text-gray-800">
             <input
               type="email"
-              placeholder="Email"
-              className="w-full p-2 text-gray-800 border rounded"
+              placeholder="sample@email.com"
+              className="w-full p-2 text-sm border rounded-md"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
 
             <input
               type="password"
-              placeholder="Password"
-              className="w-full p-2 text-gray-800 border rounded"
+              placeholder="Create password"
+              className="w-full p-2 text-sm border rounded-md"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Confirm password"
+              className="w-full p-2 text-sm border rounded-md"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
